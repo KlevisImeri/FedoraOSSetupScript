00cb399736a0df3bb1457191e1606eac5ecb0991
Klevis Imeri <klevisimeri11@gmail.com>
2025-08-27 00:02:00 +0200

    Added indenpotency for the sciprt

--- setup.sh
+++ setup.sh
@@ -1,41 +1,58 @@
-#### NEOVIM ALACRITTY I3 BASHRC SSH
+sudo dnf update
 
-ssh-keygen -t ed25519 -C "githubklevisimeri"
-echo "SSH key for KlevisImeri github:"
-cat .ssh/id_ed25519.pub
-read -p "Press Enter to continue..."
+if [ ! -f ~/.ssh/id_ed25519 ]; then
+    ssh-keygen -t ed25519 -C "githubklevisimeri"
+    echo "SSH key for KlevisImeri github:"
+    cat ~/.ssh/id_ed25519.pub
+    read -p "Press Enter to continue..."
+fi
 
-sudo dnf install -y neovim python3-neovim
-sudo dnf install alacritty
+sudo dnf install -y \
+    neovim \
+    python3-neovim \
+    alacritty \
+    git \
+    chromium-browser \
+    pip \
+    age \
+    flameshot \
+    adb \
+    pactl \
+    python3
 
-cd .config/
-git clone git@github.com:KlevisImeri/nvim
+pip install pulsemixer
+
+mkdir -p ~/.config
+cd ~/.config/
+rm -rf nvim
+git clone git@github.com:KlevisImeri/nvim.git
+rm -rf alacritty
 git clone git@github.com:KlevisImeri/alacritty.git
-rm -r i3
-git@github.com:KlevisImeri/i3.git
-cd ..
+rm -rf i3
+git clone git@github.com:KlevisImeri/i3.git
+cd ~
 
-cd Downloads
-curl https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
+mkdir -p ~/Downloads
+cd ~/Downloads
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
 unzip FiraCode.zip
 mkdir -p ~/.local/share/fonts
 mv FiraCodeNerdFont*.ttf ~/.local/share/fonts/
 fc-cache -fv
-cd ..
+cd ~
 
-mkdir Project
-cd Project
+mkdir -p ~/Project
+cd ~/Project
+
+rm -rf bashrc
 git clone git@github.com:KlevisImeri/bashrc.git
 cd bashrc
 mv .bashrc ~/
 mv .git-prompt.sh ~/
+mkdir -p ~/.ssh
 mv .ssh/config ~/.ssh/
-cd ..
+sudo mv audiosource.sh /usr/local/bin/
+cd ~
 
-### OTHER
-sudo dnf install chromium-browser pip age
-pip install pulsemixer
-
-
-### FINAL
 i3-msg restart
+source ~/.bashrc
